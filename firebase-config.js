5ff0e2ccdc51af74fce909fe49d073f4b9ae48d7
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2026-06-05 21:47:22 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA8rEqkC8m8bmDi9UhnJuGuTqaWXY5CuKM",
+  apiKey: "AIzaSyC7_6GCYy3rBPSDGOiyaEYFp-QkHik2EI0",
   authDomain: "filehub-counter.firebaseapp.com",
-  projectId: "filehub-counter"
+  projectId: "filehub-counter",
+  storageBucket: "filehub-counter.firebasestorage.app",
+  messagingSenderId: "696175439521",
+  appId: "1:696175439521:web:1a6f3e8ec8fbae08bd4ae0",
+  measurementId: "G-854HCGTN6Y"
 };
